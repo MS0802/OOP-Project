a7537b8d92a5d41c57fdcb3073e03627f96ca71c
SimpleX6-del <shaheerzeeshan6@gmail.com>
2026-03-24 23:49:21 +0500

    unique ptr and update incomplete

--- Enemy.cpp
+++ Enemy.cpp
@@ -3,11 +3,28 @@ protected:
     int attackPower;
 
 public:
-    Enemy(string n ,string typ, position _pos, int h,  int atk)
-        :Entity(n, typ, _pos,h){   
-            attackPower = atk;
+    Enemy(int startX, int startY, int h,int maxh, int atk)
+    :Entity(startX,startY,h,maxh){
+        attackPower = atk;
     }
 
-    void update() override;
-    void attack(Entity& target);
+    void update(int X,int Y) override{//Will probably be used to implement player tracking AI
+
+    }
+    char getSymbol()const override{
+        return 'E';
+    }
+    
+    void attack(Entity& target){
+        if (attackPower > target.getHP()){
+            target.sethp(0);
+
+        }
+    }
+
+    int getAttackPower()const{
+        return attackPower;
+    }
+
+    virtual std::unique_ptr<Item> dropItem();//returns nullptr by default
 };
