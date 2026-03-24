7339137605663895eb871da59f62df57ff7bb43e
SimpleX6-del <shaheerzeeshan6@gmail.com>
2026-03-24 23:16:16 +0500

    Player.h

--- Player.h
+++ Player.h
@@ -10,15 +10,9 @@ private:
     int attackPower;
 
 public:
-    Player(string name, string type , position _pos, int health,int attackp)
-    :Entity(name,type,_pos,health){
-        attackPower = attackp;
-    }
+    Player(int x, int y,int health, int maxhealth ,int attackp){}
 
-    void update(int pX,int pY) override{
-        p.x = pX;
-        p.y = pY;
-    }
+    void update(int pX,int pY) override{}
 
     char getSymbol()const override;
 
@@ -26,27 +20,11 @@ public:
     void useItem(int index);
     void addItem(std::unique_ptr<Item> item); //Takes ownership of items using move
 
-    void heal(int amount){
-        if (amount >=0){
-            health += amount;
-        }
+    void heal(int amount){}
 
-        //exception handling to be applied
-    }
-
-    int getXP()const{
-        return XP;
-    }
-    int getAttackPower()const{
-        return attackPower;
-    }
+    int getXP()const{}
+    int getAttackPower()const{}
     int getInventorySize()const;
 
-    void setXP(int ammount){
-        if (amount >=0){
-            XP+= amount;
-        }
-
-        //exception hadling
-    }
+    void setXP(int amount){}
 };
