8ad6a7fbcf931e623dbb875e1fb345ef86930b9d
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-15 15:07:40 +0500

    Created Item.cpp

---
+++ Item.cpp
@@ -0,0 +1,15 @@
+#include"Item.h"
+
+Item::Item(const std::string& itemName, ItemType type, std::string ID, int StackSize) : 
+name(itemName), type(type), id(ID), stackSize(StackSize) {}
+
+Item::Item(ItemType type) : 
+type(type), name(), id(), stackSize(0) {}
+
+std::string Item::Name() const { return name; }
+std::string Item::ID() const { return id; }
+size_t Item::StackSize() const { return stackSize; }
+ItemType Item::Type() const { return type; }
+
+ItemStack::ItemStack(std::unique_ptr<Item> item, size_t size) : 
+item(std::move(item)), size(size) {}
