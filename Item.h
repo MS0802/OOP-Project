5dd1936ca0e4e73ac9f4a14e77295cd30d3cc995
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-15 15:05:35 +0500

    Updated Item.h

--- Item.h
+++ Item.h
@@ -1,19 +1,38 @@
-#ifndef ITEM_H
-#define ITEM_H
+#pragma once
+#include<string>
+#include<memory>
 
-#include <string>
 class Player;
 
-class Item {
-protected:
-    std::string name;
-
-public:
-    Item(const std::string& itemName);
-    virtual ~Item() {}
-    
-    const std::string& getName()const;//will probably use in render for inventory display
-    virtual void use(Player& player) = 0;
+enum class ItemType {
+    UTILITY,
+    POTION,
+    WEAPON,
+    TOOL
 };
 
-#endif
+class Item {
+    protected:
+        std::string name;
+        ItemType type;
+        std::string id;
+        size_t stackSize;
+
+        Item(const std::string& itemName, ItemType type, std::string ID, int StackSize);
+        Item(ItemType type);
+    public:
+        virtual ~Item() {}
+        
+        std::string Name() const; //will probably use in render for inventory display
+        std::string ID() const;
+        size_t StackSize() const;
+        ItemType Type() const;
+        
+        virtual void use(Player& player) = 0;
+};
+
+struct ItemStack {
+    std::unique_ptr<Item> item;
+    size_t size;
+    ItemStack(std::unique_ptr<Item> item, size_t size);
+};
