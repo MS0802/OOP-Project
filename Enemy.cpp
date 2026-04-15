1320d37d1f2b7c26422fbc76892d65d792fef507
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-15 15:04:59 +0500

    Updated Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,33 +1,43 @@
-class Enemy : public Entity {
-protected:
-    int attackPower;
+#include"Enemy.h"
 
-public:
-    Enemy(int startX, int startY, int h,int maxh, int atk)
-    :Entity(startX,startY,h,maxh){
-        attackPower = atk;
+Enemy::Enemy(std::string name, int startX, int startY, size_t hp, size_t MaxHP, int def, std::array<std::unique_ptr<Item>, 3> drops, size_t XP, int damage) : 
+Entity(name, startX, startY, hp, MaxHP, def), xp(XP), drops(std::move(drops)), dmg(damage) {}
+
+void Enemy::update(int x, int y) {
+    //Will probably be used to implement player tracking AI
+    //for now:
+    position.x += x;
+    position.y += y;
+}
+
+char Enemy::getSymbol() const { return 'E'; }
+int Enemy::Damage() const { return dmg; }
+int Enemy::Defense() const { return def; }
+size_t Enemy::XP() const { return xp; }
+
+void Enemy::attack(Entity& target) { target.takeDamage(Damage()); }
+
+std::array<std::unique_ptr<Item>, 3> Enemy::dropItem() {
+    std::array<std::unique_ptr<Item>, 3> RET;
+    int items;
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, 3);
+    items = dist(gen);
+
+    for(int i = 0; i<items; i++) {
+        RET[i] = std::move(drops[i]);
     }
 
-    void update(int X,int Y) override{//Will probably be used to implement player tracking AI
+    return RET;
+}
 
-    }
-    char getSymbol()const override{
-        return 'E';
-    }
-    
-    void attack(Entity& target){
-        if (attackPower > target.getHP()){
-            target.sethp(0);
-            cout<<"player died";
+void Enemy::Effect_Action() {
+    for(auto& [type, e] : effects) {
+        e.Time()--;
+        switch(type) {
+            case EffectType::HEAL:
+                break;
         }
-        else {
-            target.sethp(target.getHP() - attackPower);
-        }
     }
-
-    int getAttackPower()const{
-        return attackPower;
-    }
-
-    virtual std::unique_ptr<Item> dropItem();//returns nullptr by default
-};
+}
