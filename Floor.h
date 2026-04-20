778aede2770ceaf48d4e46b90055ecfe91e399bf
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-20 15:29:55 +0500

    Update Floor.h

--- Floor.h
+++ Floor.h
@@ -1,36 +1,15 @@
-#ifndef FLOOR_H
-#define FLOOR_H
-
-#include "Zone.h"
-#include "Enemy.h"
-#include <vector>
-#include <string>
+#pragma once
+#include"Zone.h"
 
 class Floor{
-    private:
-    std::vector<std::vector<Zone>> zones;
-    std::vector<Enemy*> enemies; //global coordinates (not per zone)
-    int zonesWide, zonesHigh;
-    int floorNumber;
-    
-
+    protected:
+        int floorNumber;
+        std::vector<Zone> zones;
+        std::unique_ptr<Zone> activeZone;
     public:
-    Floor(const std::string& filename,int floorNum) {};
-    ~Floor(){};
-
-    Tile& getTile(int globalX,int globalY);
-    bool isWalkable(int globalX,int globalY);
-
-    Zone& getZone(int zx, int zy);
-    Zone& getZoneAt(int globalX,int globalY);
-
-    std::vector<Enemy*>& getEnemies();
-    void addEnemy(Enemy* e);
-    void removeDeadEnemies();
-
-    int getFloorNumber() const;
-
-
+        Floor(const std::vector<Zone>& Zones, int FloorNumber, std::unique_ptr<Zone> ActiveZone);
+        Floor(int FloorNumber);
+        
+        std::unique_ptr<Zone>& ActiveZone();
+        int FloorNumber() const;
 };
-
-#endif
