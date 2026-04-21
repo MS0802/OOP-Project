60a8d17fd097471e12abcf9feaa1996910535433
BladedXeno <rahimkara147@gmail.com>
2026-04-21 22:40:13 +0500

    Updated Zone.h

--- Zone.h
+++ Zone.h
@@ -2,6 +2,7 @@
 #include"Tile.h"
 #include"entity.h"
 #include"Vector2.h"
+#include"Breakable.h"
 #include<vector>
 #include<memory>
 
@@ -11,8 +12,15 @@ class Zone{
         size_t zoneWidth;
         std::vector<std::vector<std::unique_ptr<Tile>>> tiles;
         std::vector<std::unique_ptr<Entity>> entities;
+        std::random_device rd;
+        std::mt19937 gen;
+
+        bool isIsolated(int x, int y) const;
+        bool EntityPresent(int x, int y) const;
     public:
         Zone(size_t length, size_t width, const std::vector<std::vector<std::unique_ptr<Tile>>>& Tiles, const std::vector<std::unique_ptr<Entity>>& Entities);
+
+        Zone(size_t length, size_t width);
 
         std::unique_ptr<Tile> getTile(int x, int y);
 
@@ -21,5 +29,7 @@ class Zone{
 
         Vector2 Center() const;
 
-        //rest of the functions are not complete yet
+        void ResourcePopulate(int Resource_Amount, ResourceType Type);
+
+        void EntityPopulate(int Entity_Amount, std::unique_ptr<Entity> entity);
 };
