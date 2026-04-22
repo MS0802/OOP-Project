467c33806a3b47cf26883019523dfa79e0d706bd
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 11:25:41 +0500

    Updated Floor.h

--- Floor.h
+++ Floor.h
@@ -1,15 +1,20 @@
 #pragma once
 #include"Zone.h"
+#include"Entrance.h"
 
 class Floor{
     protected:
         int floorNumber;
         std::vector<Zone> zones;
         std::unique_ptr<Zone> activeZone;
+        const int numberOfZones;
     public:
         Floor(const std::vector<Zone>& Zones, int FloorNumber, std::unique_ptr<Zone> ActiveZone);
-        Floor(int FloorNumber);
+        Floor(int FloorNumber, int MAX_Zones);
         
         std::unique_ptr<Zone>& ActiveZone();
         int FloorNumber() const;
+
+        void AddZone(size_t ZoneLength, size_t ZoneWidth);
+        void ZoneBoundary_andExits();
 };
