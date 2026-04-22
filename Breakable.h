f7a59ab961a918905dd9757f62197ca38e58c967
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 11:16:18 +0500

    Updated Breakable.h

--- Breakable.h
+++ Breakable.h
@@ -1,6 +1,15 @@
 #include"Tile.h"
 
+enum class ResourceType {
+    TREE,
+    STONE,
+    IRON,
+    TITANIUM
+};
+
 class Breakable : public Tile {
+    protected:
+        ResourceType type;
     public:
-        Breakable();
+        Breakable(ResourceType Type);
 };
