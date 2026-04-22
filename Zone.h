6e5ba5b2b005e1f30f3e609ac7abd2f331f67ad9
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 11:17:36 +0500

    Updated Zone.h

--- Zone.h
+++ Zone.h
@@ -3,13 +3,17 @@
 #include"Entity.h"
 #include"Vector2.h"
 #include"Breakable.h"
+#include"Unbreakable.h"
 #include<vector>
 #include<memory>
+#include<random>
+
+class Floor;
 
 class Zone{
     private:
-        size_t zoneLength;
-        size_t zoneWidth;
+        const size_t zoneLength;
+        const size_t zoneWidth;
         std::vector<std::vector<std::unique_ptr<Tile>>> tiles;
         std::vector<std::unique_ptr<Entity>> entities;
         std::random_device rd;
@@ -17,6 +21,7 @@ class Zone{
 
         bool isIsolated(int x, int y) const;
         bool EntityPresent(int x, int y) const;
+        bool isFilled(int x, int y) const;
     public:
         Zone(size_t length, size_t width, const std::vector<std::vector<std::unique_ptr<Tile>>>& Tiles, const std::vector<std::unique_ptr<Entity>>& Entities);
 
@@ -29,7 +34,11 @@ class Zone{
 
         Vector2 Center() const;
 
+        void ZoneBoundary();
+
         void ResourcePopulate(int Resource_Amount, ResourceType Type);
 
         void EntityPopulate(int Entity_Amount, std::unique_ptr<Entity> entity);
+
+        friend class Floor;
 };
