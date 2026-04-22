ab562012f0c448fd0a487b829ddc5d8575303797
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 15:39:41 +0500

    Updated Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,9 +1,9 @@
 #include"Entity.h"
 
 Entity::Entity(std::string Name, int startX, int startY, int HP, int MaxHP, int def) : 
-name(Name), position(startX, startY), hp(HP), maxHP(MaxHP), defense(def), maxDefense(def) {}
+name(Name), position(startX, startY), hp(HP), maxHP(MaxHP), defense(def), initialDefense(def) {}
 
-Entity::Entity() : position(0, 0), name(), effects(), maxHP(0), hp(0), defense(0), maxDefense(0) {}
+Entity::Entity() : position(0, 0), name(), effects(), maxHP(0), hp(0), defense(0), initialDefense(0) {}
 
 void Entity::move(int pX, int pY) {
     position.x += pX;
@@ -35,7 +35,7 @@ int Entity::MaxHP() const { return maxHP; }
 std::string Entity::Name() const { return name; }
 bool Entity::isAlive() const { return (hp > 0); }
 int Entity::Defense() const { return defense; }
-int Entity::MaxDefense() const { return maxDefense; }
+int Entity::Initial_Defense() const { return initialDefense; }
 void Entity::Effect_Add(Effect other) {
     if(effects.count(other.Effect_Give())) {
         effects[other.Effect_Give()].Time() += other.Time();
