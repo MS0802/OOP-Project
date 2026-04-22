25d34c0423cab887723e2af7f71ef0d49de862c0
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 13:56:42 +0500

    Updated Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,9 +1,9 @@
 #include"Entity.h"
 
-Entity::Entity(std::string name, int startX, int startY, size_t HP, size_t MaxHP, int def) : 
-Name(name), position(startX, startY), hp(HP), maxHP(MaxHP), def(def) {}
+Entity::Entity(std::string Name, int startX, int startY, size_t HP, size_t MaxHP, int def) : 
+name(Name), position(startX, startY), hp(HP), maxHP(MaxHP), def(def) {}
 
-Entity::Entity() : position(0, 0), Name(), effects(), maxHP(0), hp(0) {}
+Entity::Entity() : position(0, 0), name(), effects(), maxHP(0), hp(0) {}
 
 void Entity::move(int pX, int pY) {
     position.x += pX;
@@ -32,6 +32,7 @@ int Entity::PosX() const { return position.x; }
 int Entity::posY() const { return position.y; }
 size_t Entity::HP() const { return hp; }
 size_t Entity::MaxHP() const { return maxHP; }
+std::string Entity::Name() const { return name; }
 bool Entity::isAlive() const { return (hp > 0); }
 void Entity::Effect_Add(Effect other) {
     if(effects.count(other.Effect_Give())) {
@@ -41,5 +42,3 @@ void Entity::Effect_Add(Effect other) {
         effects[other.Effect_Give()] = other;
     }
 }
-
-//My edit
