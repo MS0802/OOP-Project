2bfe910e85b364b9045dfd242f90bb714318d0f3
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 15:35:43 +0500

    Updated Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -8,20 +8,21 @@
 class Enemy : public Entity {
     protected:
         std::array<std::unique_ptr<Item>, 3> drops;
-        size_t xp;
-        int dmg;
+        int xp;
+        int damage;
+        const int initialDamage;
     public:
-        Enemy(std::string name, int startX, int startY, size_t hp, size_t MaxHP, int def, std::array<std::unique_ptr<Item>, 3> drops, size_t XP, int damage);
+        Enemy(std::string Name, int startX, int startY, int HP, int MaxHP, int Defense, std::array<std::unique_ptr<Item>, 3> Drops, int XP, int Damage);
+        Enemy();
         void update(int x, int y) override;
         char getSymbol() const override;
 
         void attack(Entity& target);
         int Damage() const override;
-        int Defense() const override;
+        int Initial_Damage() const;
         size_t XP() const;
 
         void Effect_Action() override;
-
 
         virtual std::array<std::unique_ptr<Item>, 3> dropItem();
 };
