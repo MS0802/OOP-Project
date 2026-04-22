a40f98bb616e5f70f5af7b4aa3834b61969afa69
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-04-22 15:36:26 +0500

    Updated Boss.h

--- Boss.h
+++ Boss.h
@@ -2,10 +2,14 @@
 #include"Enemy.h"
 
 class Boss : public Enemy {
+    protected:
+        const int regenerationFactor;
     public:
-        Boss(std::string name, int startX, int startY, size_t hp, size_t MaxHP, int def, std::array<std::unique_ptr<Item>, 3> drops, size_t XP, int damage);
+        Boss(std::string name, int startX, int startY, size_t hp, size_t MaxHP, int def, std::array<std::unique_ptr<Item>, 3> drops, size_t XP, int damage, int Regeneration_Factor);
 
         char getSymbol()const override;
+
         void specialAttack(Entity& target);
+
         std::array<std::unique_ptr<Item>, 3> dropItem() override;
 };
