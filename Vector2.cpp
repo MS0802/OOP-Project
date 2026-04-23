c2ffafe1113c0694263dd172e9b1d88efeb47625
BladedXeno <rahimkara147@gmail.com>
2026-04-23 18:21:52 +0500

    Updated Vector2.cpp

--- Vector2.cpp
+++ Vector2.cpp
@@ -3,10 +3,13 @@
 Vector2::Vector2(int X, int Y) : 
 x(X), y(Y) {}
 
-Vector2& Vector2::operator+(const Vector2& other) {
+Vector2::Vector2(const Vector2& other) : 
+x(other.x), y(other.y) {}
+
+Vector2 Vector2::operator+(const Vector2& other) const {
     return Vector2(this->x + other.x, this->y + other.y);
 }
 
-Vector2& Vector2::operator-(const Vector2& other) {
+Vector2 Vector2::operator-(const Vector2& other) const {
     return Vector2(this->x - other.x, this->y - other.y);
 }
