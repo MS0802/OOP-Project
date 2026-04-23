c2ca86aa01e3a71992465ffb62c5f62ae8dbbd3c
BladedXeno <rahimkara147@gmail.com>
2026-04-23 18:14:15 +0500

    Updated Boss.h

--- Boss.h
+++ Boss.h
@@ -6,6 +6,7 @@ class Boss : public Enemy {
         const int regenerationFactor;
     public:
         Boss(std::string name, int startX, int startY, size_t hp, size_t MaxHP, int def, std::array<std::unique_ptr<Item>, 3> drops, size_t XP, int damage, int Regeneration_Factor);
+        Boss(const Boss& other);
 
         char getSymbol()const override;
 
