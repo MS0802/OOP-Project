377f8ecb58fba0ad3f1c29845a19ff58a1185d7e
BladedXeno <rahimkara147@gmail.com>
2026-04-23 18:22:26 +0500

    Updated Vector2.h

--- Vector2.h
+++ Vector2.h
@@ -2,6 +2,8 @@ struct Vector2 {
     int x;
     int y;
     Vector2(int X, int Y);
-    Vector2& operator+(const Vector2& other);
-    Vector2& operator-(const Vector2& other);
+    Vector2(const Vector2& other);
+    
+    Vector2 operator+(const Vector2& other) const;
+    Vector2 operator-(const Vector2& other) const;
 };
