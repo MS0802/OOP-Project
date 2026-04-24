af0b3628145afa9139e3d5d734df83ed2b7abfa7
BladedXeno <rahimkara147@gmail.com>
2026-04-24 10:46:24 +0500

    Updated Tool.cpp

--- Tool.cpp
+++ Tool.cpp
@@ -1,12 +1,19 @@
 #include"Tool.h"
 #include"Player.h"
 
-Tool::Tool(const std::string& ToolName, std::string ID, int StackSize, double Durability, const std::vector<std::unique_ptr<Item>>& Can_Break) : 
-Item(ToolName, ItemType::TOOL, ID, StackSize), durability(Durability), can_break(Can_Break) {}
+Tool::Tool(const std::string& ToolName, std::string ID, int StackSize, double Durability, const std::vector<std::string>& CanBreakIDs) : 
+Item(ToolName, ItemType::TOOL, ID, StackSize), durability(Durability), canBreakIDs(CanBreakIDs) {}
+
+Tool::Tool(const Tool& other) : 
+Item(other), durability(other.durability), canBreakIDs(other.canBreakIDs) {}
 
 Tool::Tool() : 
-Item(ItemType::TOOL), durability(0), can_break() {}
+Item(ItemType::TOOL), durability(0), canBreakIDs() {}
 
 void Tool::use(Player& player) {
     //data
+}
+
+std::unique_ptr<Item> Tool::clone() const {
+    return std::make_unique<Tool>(*this);
 }
