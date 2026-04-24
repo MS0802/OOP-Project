50742e6309aa702849b9569dee470fcd6ac0b80a
BladedXeno <rahimkara147@gmail.com>
2026-04-24 10:47:46 +0500

    Updated Utility.h

--- Utility.h
+++ Utility.h
@@ -4,11 +4,13 @@
 
 class Utility : public Item {
     protected:
-        std::vector<std::unique_ptr<Item>> can_Craft;
+        std::vector<std::string> canCraftIDs; // Store IDs instead of unique_ptrs
     public:
-        Utility(const std::string& ItemName, std::string ID, int stackSize, std::vector<std::unique_ptr<Item>> canCraft);
+        Utility(const std::string& ItemName, std::string ID, int stackSize, const std::vector<std::string>& canCraftIDs);
         Utility(const std::string& ItemName, std::string ID, int stackSize);
+        Utility(const Utility& other);
         Utility();
 
         void use(Player& player) override;
+        std::unique_ptr<Item> clone() const override;
 };
