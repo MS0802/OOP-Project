1b50e9dca3fe1202560ec5e132fb07f99038a99b
BladedXeno <rahimkara147@gmail.com>
2026-04-24 10:48:24 +0500

    Updated Weapon.h

--- Weapon.h
+++ Weapon.h
@@ -6,11 +6,13 @@ class Weapon : public Item {
     protected:
         int damage;
         int durability;
-        std::vector<ItemStack> Recipie;
+        std::vector<ItemStack> Recipe;
     public:
-        Weapon(const std::string& name, const std::string& id, int dmg, int durability, const std::vector<ItemStack>& Recipie);
+        Weapon(const std::string& name, const std::string& id, int dmg, int durability, const std::vector<ItemStack>& Recipe);
+        Weapon(const Weapon& other);
         Weapon();
 
         void use(Player& player) override;
+        std::unique_ptr<Item> clone() const override;
         int Damage() const;
 };
