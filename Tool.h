5b1653def530b39535db4dfed42768c9ca4284d8
BladedXeno <rahimkara147@gmail.com>
2026-04-24 10:46:40 +0500

    Updated Tool.h

--- Tool.h
+++ Tool.h
@@ -5,9 +5,12 @@
 class Tool : public Item {
     protected:
         double durability;
-        std::vector<std::unique_ptr<Item>> can_break;
+        std::vector<std::string> canBreakIDs; // Store IDs instead of unique_ptrs
     public:
-        Tool(const std::string& itemName, std::string ID, int StackSize, double Durability, const std::vector<std::unique_ptr<Item>>& Can_Break);
+        Tool(const std::string& itemName, std::string ID, int StackSize, double Durability, const std::vector<std::string>& CanBreakIDs);
+        Tool(const Tool& other);
         Tool();
+        
         void use(Player& player) override;
+        std::unique_ptr<Item> clone() const override;
 };
