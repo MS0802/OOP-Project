b77974f9b9050f8cac01b7a75feb5765437f58ac
BladedXeno <rahimkara147@gmail.com>
2026-04-24 10:45:26 +0500

    Updated Potion.cpp

--- Potion.cpp
+++ Potion.cpp
@@ -1,11 +1,16 @@
 #include"Potion.h"
 #include"Player.h"
 
-Potion::Potion(const std::string& name, const std::string& id, EffectType effectType, double duration) : 
-Item(name, ItemType::POTION, id, 1), potionEffect(effectType, duration) {}
+Potion::Potion(const std::string& name, const std::string& id, EffectType effectType, double duration, bool canGiveOthers) : 
+Item(name, ItemType::POTION, id, 1), potionEffect(effectType, duration, canGiveOthers) {}
+
 Potion::Potion(EffectType type) : 
-Item(ItemType::POTION), potionEffect(type, 0) {}
+Item(ItemType::POTION), potionEffect(type, 0, false) {}
 
 void Potion::use(Player& player) {
     //logic left...
+}
+
+std::unique_ptr<Item> Potion::clone() const {
+    return std::make_unique<Potion>(*this);
 }
