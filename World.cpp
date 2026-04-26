a99ba956b815d0ba7d8504d98a2a48b195984223
BladedXeno <rahimkara147@gmail.com>
2026-04-26 23:00:01 +0500

    Updated World.cpp

--- World.cpp
+++ World.cpp
@@ -6,5 +6,11 @@ floors(7), totalFloors(7), activeFloor(nullptr) {}
 void World::SetActiveFloor(size_t FloorNumber) {
     FloorNumber--;
     FloorNumber = FloorNumber%7;
-    activeFloor; 
+    activeFloor = floors[FloorNumber].get();
+}
+
+void World::InitializeFloor1() {
+    Floor first(1, 5);
+    first.AddZone(50, 50);
+    
 }
