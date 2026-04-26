0af114ae59b1f99ae14e168d89e470cee0580218
BladedXeno <rahimkara147@gmail.com>
2026-04-26 23:02:45 +0500

    Updated Floor.cpp

--- Floor.cpp
+++ Floor.cpp
@@ -17,15 +17,20 @@ void Floor::AddZone(size_t ZoneLength, size_t ZoneWidth) {
 
 void Floor::ZoneBoundary_andExits() {
     for(auto& zone : zones) {
-        for(int j = zone.Center().x; j < zone.ZoneLength(); j++) {
-            if(j%2) { zone.tiles[j][0]->ReplaceTile(Entrance(TileType::ZONE_EXIT)); }
-            else { zone.tiles[j][zone.ZoneWidth()-1]->ReplaceTile(Entrance(TileType::ZONE_EXIT)); }
-        }
-        if(floorNumber != 1) {
-            for(int j = zone.Center().y; j < zone.ZoneWidth(); j++) {
-                if(j%2) { zone.tiles[0][j]->ReplaceTile(Entrance(TileType::ZONE_EXIT)); }
-                else { zone.tiles[zone.ZoneLength()-1][j]->ReplaceTile(Entrance(TileType::ZONE_EXIT)); }
-            }
+        zone.ZoneBoundary();
+        std::uniform_int_distribution<> sideDetermine(0, 3);
+        std::uniform_int_distribution<> midSelection(0, 3);
+        int side = sideDetermine(zone.gen);
+        switch(side) {
+            case 0:
+                
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
         }
     }
 }
