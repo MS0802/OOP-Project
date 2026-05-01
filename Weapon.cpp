ec976a8922b0d7289569e540ad49cf279ff6bcde
BladedXeno <rahimkara147@gmail.com>
2026-05-01 18:00:37 +0500

    Updated Weapon.cpp

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,14 +1,16 @@
 #include"Weapon.h"
 #include"Player.h"
 
-Weapon::Weapon(const std::string& name, const std::string& id, int dmg, int durability, const std::vector<ItemStack>& Recipe) : 
-Item(name, ItemType::WEAPON, id, 1), damage(dmg), durability(durability), Recipe(Recipe) {}
+Weapon::Weapon(const std::string& name, int dmg, int durability, const std::vector<ItemStack>& Recipe) : 
+Item(name, ItemType::WEAPON, 1), damage(dmg), durability(durability), recipe(Recipe.size()) {
+    for(int i = 0; i<Recipe.size(); i++) { recipe[i] = Recipe[i];  }
+}
 
 Weapon::Weapon(const Weapon& other) : 
-Item(other), damage(other.damage), durability(other.durability), Recipe(other.Recipe) {}
+Item(other), damage(other.damage), durability(other.durability), recipe(other.recipe) {}
 
 Weapon::Weapon() : 
-Item(ItemType::WEAPON), damage(0), durability(0), Recipe() {}
+Item(ItemType::WEAPON), damage(0), durability(0), recipe() {}
 
 void Weapon::use(Player& player) {
     //logic left...
