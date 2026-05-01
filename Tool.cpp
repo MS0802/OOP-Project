92b8cc1a0251c07617fdd4f5fa4c6fddf96fe125
BladedXeno <rahimkara147@gmail.com>
2026-05-01 17:59:24 +0500

    Updated Tool.cpp

--- Tool.cpp
+++ Tool.cpp
@@ -1,14 +1,17 @@
 #include"Tool.h"
 #include"Player.h"
 
-Tool::Tool(const std::string& ToolName, std::string ID, int StackSize, double Durability, const std::vector<std::string>& CanBreakIDs) : 
-Item(ToolName, ItemType::TOOL, ID, StackSize), durability(Durability), canBreakIDs(CanBreakIDs) {}
+Tool::Tool(const std::string& ToolName, double Durability, const std::vector<std::string>& CanBreak, std::vector<ItemStack>& Recipe) : 
+Item(ToolName, ItemType::TOOL, 1), durability(Durability), canBreak(CanBreak), recipe() {
+    recipe.resize(Recipe.size());
+    for(int i = 0; i<recipe.size(); i++) { recipe[i] = Recipe[i]; }
+}
 
 Tool::Tool(const Tool& other) : 
-Item(other), durability(other.durability), canBreakIDs(other.canBreakIDs) {}
+Item(other), durability(other.durability), canBreak(other.canBreak) {}
 
 Tool::Tool() : 
-Item(ItemType::TOOL), durability(0), canBreakIDs() {}
+Item(ItemType::TOOL), durability(0), canBreak() {}
 
 void Tool::use(Player& player) {
     //data
