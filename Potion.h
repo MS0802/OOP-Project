0987d11542f516e2165e001385d53889155b8a04
BladedXeno <rahimkara147@gmail.com>
2026-05-01 17:58:45 +0500

    Updated Potion.h

--- Potion.h
+++ Potion.h
@@ -6,9 +6,17 @@ class Potion : public Item {
     protected:
         Effect potionEffect;
     public:
-        Potion(const std::string& name, const std::string& id, EffectType effectType, double duration, bool canGiveOthers);
+        Potion(const std::string& name, EffectType effectType, double duration, bool canGiveOthers);
         Potion(EffectType type);
 
         void use(Player& player) override;
         std::unique_ptr<Item> clone() const override;
+
+        static Potion SelfHealing();
+        static Potion InflictHealing();
+        static Potion Poison();
+        static Potion Strength();
+        static Potion Weakness();
+        static Potion Resistance();
+        static Potion Vulnerability();
 };
