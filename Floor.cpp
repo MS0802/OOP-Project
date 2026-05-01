dedb9c39d290391907d3e4ed92cba37a73f04feb
BladedXeno <rahimkara147@gmail.com>
2026-05-01 17:53:27 +0500

    Updated Floor.cpp

--- Floor.cpp
+++ Floor.cpp
@@ -1,36 +1,53 @@
 #include"Floor.h"
 
-Floor::Floor(const std::vector<Zone>& Zones, int FloorNumber, std::unique_ptr<Zone> ActiveZone) : 
-zones(Zones), floorNumber(FloorNumber), activeZone(std::move(ActiveZone)), numberOfZones(Zones.size()) {}
+Floor::Floor(std::vector<std::unique_ptr<Zone>>& Zones, int FloorNumber, Zone* ActiveZone) : 
+zones(std::move(Zones)), floorNumber(FloorNumber), activeZone(ActiveZone), numberOfZones(Zones.size()) {}
 
 Floor::Floor(int FloorNumber, int MAX_Zones) : 
 floorNumber(FloorNumber), zones(), activeZone(nullptr), numberOfZones(MAX_Zones) {}
 
-Zone* Floor::ActiveZone() { return activeZone.get(); }
+Zone* Floor::ActiveZone() { return activeZone; }
 int Floor::FloorNumber() const { return floorNumber; }
+int Floor::Number_ofZones() const { return numberOfZones; }
 
 void Floor::AddZone(size_t ZoneLength, size_t ZoneWidth) {
     if(zones.size() < numberOfZones) {
-        zones.push_back(Zone(ZoneLength, ZoneWidth));
+        zones.push_back(std::make_unique<Zone>(Zone(ZoneLength, ZoneWidth)));
     }
 }
 
 void Floor::ZoneBoundary_andExits() {
     for(auto& zone : zones) {
-        zone.ZoneBoundary();
-        std::uniform_int_distribution<> sideDetermine(0, 3);
-        std::uniform_int_distribution<> midSelection(0, 3);
-        int side = sideDetermine(zone.gen);
-        switch(side) {
-            case 0:
-                
+        zone->ZoneBoundary();
+        std::vector<int> sides = {0, 1, 2, 3};
+        std::shuffle(sides.begin(), sides.end(), zone->gen);
+        
+        for(int i = 0; i<2; i++) {
+            int side = sides[i];
+            int exitCount = std::uniform_int_distribution<>(1, 4) (zone->gen);
+            int startPos, endPos;
+            
+            if(side == 0 || side == 1) {
+                int startX = std::uniform_int_distribution<>(0, zone->ZoneWidth()-1)(zone->gen);
+                startPos = startX + 2;
+                endPos = startPos + (exitCount - 1);
+                int row = ((side == 0) ? 0 : zone->ZoneLength()-1);
+                for(int x = startPos; x<=endPos; ++x) {
+                    zone->tiles[row][x]->ReplaceTile(Entrance(TileType::ZONE_EXIT));
+                }
+            }
+            else {
+                int startX = std::uniform_int_distribution<>(0, zone->ZoneLength()-1)(zone->gen);
+                startPos = startX + 2;
+                endPos = startPos + (exitCount - 1);
+                int col = ((side == 0) ? 0 : zone->ZoneWidth()-1);
+                for(int x = startPos; x<=endPos; ++x) {
+                    zone->tiles[x][col]->ReplaceTile(Entrance(TileType::ZONE_EXIT));
+                }
+            }
+            if(floorNumber == 1) {
                 break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
+            }
         }
     }
 }
