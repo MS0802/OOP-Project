00f65dc2156aa50a9c18f33bfe64887d135db156
BladedXeno <rahimkara147@gmail.com>
2026-05-03 14:45:48 +0500

    Updated World.cpp

--- World.cpp
+++ World.cpp
@@ -10,31 +10,20 @@ void World::SetActiveFloor(size_t FloorNumber) {
 }
 
 void World::InitializeFloor1() {
-    Floor first(1, 5);
-    first.AddZone(48, 48);
+    Floor first(1, 2);
     first.AddZone(36, 48);
-    first.AddZone(48, 54);
-    first.AddZone(18, 18);
-    first.AddZone(36, 36);
+    first.AddZone(48, 48);
     first.ZoneBoundary_andExits();
     first.activeZone = first.zones[0].get();
     for(int i = 0; i<first.Number_ofZones(); i++) {
-        if(i == 0) { first.zones[i]->ResourcePopulate(15, ResourceType::TREE); }
-        else if(i == 1) {
-            //build unbreakable stone cave walls
-            first.zones[i]->ResourcePopulate(8, ResourceType::STONE);
-            first.zones[i]->ResourcePopulate(3, ResourceType::IRON);
+        if(i == 0) {
+            first.zones[i]->ResourcePopulate(8, ResourceType::TREE);
+            first.zones[i]->ResourcePopulate(5, ResourceType::BARREL0);
         }
-        else if(i == 2) {
-            //build a cavern
+        else {
             first.zones[i]->ResourcePopulate(15, ResourceType::TREE);
-            first.zones[i]->ResourcePopulate(5, ResourceType::STONE);
-        }
-        else if(i == 3) {
-            //build shop tiles and walls
-        }
-        else if(i == 4) {
-            //build dungeon walls & rooms
+            first.zones[i]->ResourcePopulate(4, ResourceType::STONE);
+            first.zones[i]->ResourcePopulate(5, ResourceType::BARREL1);
         }
     }
 }
