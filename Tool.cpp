378f352d038f5ff64f98aa570a375cf9a8cb88b0
BladedXeno <rahimkara147@gmail.com>
2026-05-03 14:37:52 +0500

    Updated Tool.cpp

--- Tool.cpp
+++ Tool.cpp
@@ -1,7 +1,7 @@
 #include"Tool.h"
-#include"Player.h"
+#include"Breakable.h"
 
-Tool::Tool(const std::string& ToolName, double Durability, const std::vector<std::string>& CanBreak, std::vector<ItemStack>& Recipe) : 
+Tool::Tool(const std::string& ToolName, int Durability, const std::vector<ResourceType>& CanBreak, std::vector<ItemStack>& Recipe) : 
 Item(ToolName, ItemType::TOOL, 1), durability(Durability), canBreak(CanBreak), recipe() {
     recipe.resize(Recipe.size());
     for(int i = 0; i<recipe.size(); i++) { recipe[i] = Recipe[i]; }
@@ -13,10 +13,66 @@ Item(other), durability(other.durability), canBreak(other.canBreak) {}
 Tool::Tool() : 
 Item(ItemType::TOOL), durability(0), canBreak() {}
 
-void Tool::use(Player& player) {
-    //data
-}
-
 std::unique_ptr<Item> Tool::clone() const {
     return std::make_unique<Tool>(*this);
+}
+
+Tool Tool::WoodenAxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Wooden Axe", DefaultToolHP, CanBreak, Recipe);
+}
+
+Tool Tool::WoodenPickaxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Wooden Pickaxe", DefaultToolHP, CanBreak, Recipe);
+}
+
+Tool Tool::StoneAxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Stone Axe", DefaultToolHP*2, CanBreak, Recipe);
+}
+
+Tool Tool::StonePickaxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Stone Pickaxe", DefaultToolHP*2, CanBreak, Recipe);
+}
+
+Tool Tool::IronAxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Iron Axe", DefaultToolHP*4, CanBreak, Recipe);
+}
+
+Tool Tool::IronPickaxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Iron Pickaxe", DefaultToolHP*4, CanBreak, Recipe);
+}
+
+Tool Tool::TitaniumAxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Titanium Axe", DefaultToolHP*5, CanBreak, Recipe);
+}
+
+Tool Tool::TitaniumPickaxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Titanium Pickaxe", DefaultToolHP*5, CanBreak, Recipe);
+}
+
+Tool Tool::EtheriteAxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Etherite Axe", DefaultToolHP*6, CanBreak, Recipe);
+}
+
+Tool Tool::EtheritePickaxe() {
+    std::vector<ItemStack> Recipe({});
+    std::vector<ResourceType> CanBreak({});
+    return Tool("Etherite Pickaxe", DefaultToolHP*6, CanBreak, Recipe);
 }
