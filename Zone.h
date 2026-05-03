90cbfd2197cb0e6ac17c011b53e3299f4a616084
BladedXeno <rahimkara147@gmail.com>
2026-05-03 23:53:08 +0500

    Updated Zone.h

--- Zone.h
+++ Zone.h
@@ -7,27 +7,30 @@
 #include<vector>
 #include<memory>
 #include<random>
+#include<functional>
 
 class Floor;
 
 class Zone{
-    private:
+    bool isIsolated(int x, int y) const;
+    bool EntityPresent(int x, int y) const;
+    bool isFilled(int x, int y) const;
+
+    bool IsNearWall(int x, int y) const;
+    void ClearNearExit(int x, int y);
+    protected:
         const size_t zoneLength;
         const size_t zoneWidth;
         std::vector<std::vector<std::unique_ptr<Tile>>> tiles;
         std::vector<std::unique_ptr<Entity>> entities;
         std::random_device rd;
         std::mt19937 gen;
-
-        bool isIsolated(int x, int y) const;
-        bool EntityPresent(int x, int y) const;
-        bool isFilled(int x, int y) const;
     public:
         Zone(size_t length, size_t width, std::vector<std::vector<std::unique_ptr<Tile>>> Tiles, std::vector<std::unique_ptr<Entity>> Entities);
 
         Zone(size_t length, size_t width);
 
-        std::unique_ptr<Tile> getTile(int x, int y);
+        Tile* getTile(int x, int y);
 
         int ZoneLength() const;
         int ZoneWidth() const;
@@ -38,7 +41,9 @@ class Zone{
 
         void ResourcePopulate(int Resource_Amount, ResourceType Type);
 
-        void EntityPopulate(int Entity_Amount, Entity* entity);
+        void EntityPopulate(int Entity_Amount, std::function<std::unique_ptr<Entity>(int, int)> entityFactory);
+
+        void WallPopulate(int Number_of_Walls, TileType type = TileType::GROUND);
 
         friend class Floor;
 };
