90675d5e8684c32c23ea41866ddaea1211cf7ace
BladedXeno <rahimkara147@gmail.com>
2026-05-03 14:39:33 +0500

    Updated Utility.h

--- Utility.h
+++ Utility.h
@@ -4,13 +4,18 @@
 
 class Utility : public Item {
     protected:
-        std::vector<std::string> canCraft; // Store Names instead of unique_ptrs
+        std::vector<std::string> canCraft;
     public:
         Utility(const std::string& ItemName, int stackSize, const std::vector<std::string>& CanCraft);
         Utility(const std::string& ItemName, int stackSize);
         Utility(const Utility& other);
         Utility();
 
-        void use(Player& player) override;
         std::unique_ptr<Item> clone() const override;
+
+        static Utility Wood();
+        static Utility Stone();
+        static Utility Iron();
+        static Utility Titanium();
+        static Utility Etherite();
 };
