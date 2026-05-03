5a40116cd292ce44220bc0393ed45f6aabcb222e
BladedXeno <rahimkara147@gmail.com>
2026-05-03 14:37:17 +0500

    Updated Weapon.cpp

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,8 +1,7 @@
 #include"Weapon.h"
-#include"Player.h"
 
-Weapon::Weapon(const std::string& name, int dmg, int durability, const std::vector<ItemStack>& Recipe) : 
-Item(name, ItemType::WEAPON, 1), damage(dmg), durability(durability), recipe(Recipe.size()) {
+Weapon::Weapon(const std::string& name, int Damage, int durability, const std::vector<ItemStack>& Recipe) : 
+Item(name, ItemType::WEAPON, 1), damage(Damage), durability(durability), recipe(Recipe.size()) {
     for(int i = 0; i<Recipe.size(); i++) { recipe[i] = Recipe[i];  }
 }
 
@@ -12,12 +11,58 @@ Item(other), damage(other.damage), durability(other.durability), recipe(other.re
 Weapon::Weapon() : 
 Item(ItemType::WEAPON), damage(0), durability(0), recipe() {}
 
-void Weapon::use(Player& player) {
-    //logic left...
-}
-
 std::unique_ptr<Item> Weapon::clone() const {
     return std::make_unique<Weapon>(*this);
 }
 
 int Weapon::Damage() const { return damage; }
+
+Weapon Weapon::WoodenSword() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Wooden Sword", DefaultSwordDmg, DefaultSwordHP, Recipe);
+}
+
+Weapon Weapon::WoodenSpear() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Wooden Spear", DefaultSpearDmg, DefaultSpearHP, Recipe);
+}
+
+Weapon Weapon::StoneSword() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Stone Sword", DefaultSwordDmg*2, DefaultSwordHP*2, Recipe);
+}
+
+Weapon Weapon::StoneSpear() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Stone Spear", DefaultSpearDmg*2, DefaultSpearHP*2, Recipe);
+}
+
+Weapon Weapon::IronSword() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Iron Sword", DefaultSwordDmg*4, DefaultSwordHP*4, Recipe);
+}
+
+Weapon Weapon::IronSpear() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Iron Spear", DefaultSpearDmg*4, DefaultSpearHP*4, Recipe);
+}
+
+Weapon Weapon::TitaniumSword() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Titanium Sword", DefaultSwordDmg*5, DefaultSwordHP*5, Recipe);
+}
+
+Weapon Weapon::TitaniumSpear() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Titanium Spear", DefaultSpearDmg*5, DefaultSpearHP*5, Recipe);
+}
+
+Weapon Weapon::EtheriteSword() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Etherite Sword", DefaultSwordDmg*7, DefaultSwordHP*6, Recipe);
+}
+
+Weapon Weapon::EtheriteSpear() {
+    std::vector<ItemStack> Recipe({});
+    return Weapon("Etherite Spear", DefaultSpearDmg*7, DefaultSpearHP*6, Recipe);
+}
