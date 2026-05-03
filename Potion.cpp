bbacd294a5b6eeaa9347774557f6601ba0c44ec4
BladedXeno <rahimkara147@gmail.com>
2026-05-03 23:49:53 +0500

    Updated Potion.cpp

--- Potion.cpp
+++ Potion.cpp
@@ -8,7 +8,7 @@ Potion::Potion(EffectType type) :
 Item(ItemType::POTION), potionEffect(type, 0, false) {}
 
 void Potion::use(Player& player) {
-    //logic left...
+    player.Effect_Add(potionEffect);
 }
 
 std::unique_ptr<Item> Potion::clone() const {
@@ -17,8 +17,8 @@ std::unique_ptr<Item> Potion::clone() const {
 
 Potion Potion::SelfHealing() { return Potion("Healing Potion", EffectType::HEAL, 10, false); }
 Potion Potion::InflictHealing() { return Potion("Inflicting Healing Potion", EffectType::HEAL, 1, true); }
-Potion Potion::Poison() { return Potion("Poison Potion", EffectType::POISON, 1, true); }
+Potion Potion::Poison() { return Potion("Poison Potion", EffectType::POISON, 10, true); }
 Potion Potion::Strength() { return Potion("Strength Potion", EffectType::STRENGTH, 10, false); }
-Potion Potion::Weakness() { return Potion("Weakness Potion", EffectType::WEAKNESS, 1, true); }
+Potion Potion::Weakness() { return Potion("Weakness Potion", EffectType::WEAKNESS, 10, true); }
 Potion Potion::Resistance() { return Potion("Resistance Potion", EffectType::RESISTANCE, 10, false); }
-Potion Potion::Vulnerability() { return Potion("Vulnerability Potion", EffectType::VULNERABLE, 1, true); }
+Potion Potion::Vulnerability() { return Potion("Vulnerability Potion", EffectType::VULNERABLE, 10, true); }
