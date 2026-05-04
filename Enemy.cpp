30f485849d2deaf4bcc0b0f571d60b294b0b019a
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-05-04 10:25:25 +0500

    Updated Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,14 +2,14 @@
 #include"Potion.h"
 #include"Utility.h"
 
-Enemy::Enemy(std::string Name, int startX, int startY, int HP, int Defense, std::array<std::unique_ptr<Item>, 3> Drops, int XP, int Damage) : 
-Entity(Name, startX, startY, HP, Defense), xp(XP), drops(std::move(Drops)), damage(Damage), initialDamage(Damage) {}
+Enemy::Enemy(std::string Name, int startX, int startY, int HP, int Defense, std::array<std::unique_ptr<Item>, 3> Drops, int XP, int Damage, EnemyType type) : 
+Entity(Name, startX, startY, HP, Defense), xp(XP), drops(std::move(Drops)), damage(Damage), initialDamage(Damage), enemyType(type), tickCounter(0) {}
 
 Enemy::Enemy() : 
-Entity(), drops(), xp(0), damage(0), initialDamage(0) {}
+Entity(), drops(), xp(0), damage(0), initialDamage(0), enemyType(EnemyType::NORMAL), tickCounter(0) {}
 
 Enemy::Enemy(const Enemy& other) : 
-Entity(other.Name(), other.PosX(), other.PosY(), other.HP(), other.Defense()), xp(other.XP()), damage(other.Damage()), initialDamage(other.Initial_Damage()), drops() {
+Entity(other.Name(), other.PosX(), other.PosY(), other.HP(), other.Defense()), xp(other.XP()), damage(other.Damage()), initialDamage(other.Initial_Damage()), enemyType(other.Type()), tickCounter(0), drops() {
     for(int i = 0; i<3; i++) {
         if(other.drops[i]) {
             drops[i] = other.drops[i]->clone();
@@ -17,8 +17,8 @@ Entity(other.Name(), other.PosX(), other.PosY(), other.HP(), other.Defense()), x
     }
 }
 
-Enemy::Enemy(std::string Name, int startX, int startY, int HP, int Defense, int XP, int Damage, const std::array<std::unique_ptr<Item>, 3>& Drops) : 
-Entity(Name, startX, startY, HP, Defense), xp(XP), damage(Damage), initialDamage(Damage), drops() {
+Enemy::Enemy(std::string Name, int startX, int startY, int HP, int Defense, int XP, int Damage, const std::array<std::unique_ptr<Item>, 3>& Drops, EnemyType type) : 
+Entity(Name, startX, startY, HP, Defense), xp(XP), damage(Damage), initialDamage(Damage), enemyType(type), tickCounter(0), drops() {
     for(int i = 0; i<3; i++) {
         if(Drops[i]) {
             drops[i] = Drops[i]->clone();
@@ -27,16 +27,73 @@ Entity(Name, startX, startY, HP, Defense), xp(XP), damage(Damage), initialDamage
 }
 
 void Enemy::update(int x, int y) {
-    //Will probably be used to implement player tracking AI
-    //for now:
     position.x += x;
     position.y += y;
+}
+
+void Enemy::updateAI(const Entity& player) {
+    tickCounter++;
+    
+    int dx = player.PosX() - position.x;
+    int dy = player.PosY() - position.y;
+    int absDx = std::abs(dx);
+    int absDy = std::abs(dy);
+    
+    int moveX = 0, moveY = 0;
+    
+    switch(enemyType) {
+        case EnemyType::NORMAL:
+        case EnemyType::TANK:
+        case EnemyType::POISON:
+            // Move towards player (aggressive)
+            if(absDx > absDy) {
+                moveX = (dx > 0) ? 1 : -1;
+            } else if(absDy > 0) {
+                moveY = (dy > 0) ? 1 : -1;
+            }
+            break;
+            
+        case EnemyType::HEALER:
+            // Move away from player (slow - every 2 ticks)
+            if(tickCounter % 2 == 0) {
+                if(absDx < 5 || absDy < 5) {
+                    if(absDx > absDy) {
+                        moveX = (dx > 0) ? -1 : 1;
+                    } else if(absDy > 0) {
+                        moveY = (dy > 0) ? -1 : 1;
+                    }
+                }
+            }
+            break;
+            
+        case EnemyType::WIZARD:
+            // Hit once then run away
+            if(absDx <= 1 && absDy <= 1) {
+                // Too close, run away
+                if(absDx > absDy) {
+                    moveX = (dx > 0) ? -1 : 1;
+                } else {
+                    moveY = (dy > 0) ? -1 : 1;
+                }
+            } else if(absDx <= 3 && absDy <= 3) {
+                // In range for effect, move to attack
+                if(absDx > absDy) {
+                    moveX = (dx > 0) ? 1 : -1;
+                } else {
+                    moveY = (dy > 0) ? 1 : -1;
+                }
+            }
+            break;
+    }
+    
+    update(moveX, moveY);
 }
 
 char Enemy::getSymbol() const { return 'E'; }
 int Enemy::Damage() const { return damage; }
 int Enemy::Initial_Damage() const { return initialDamage; }
 size_t Enemy::XP() const { return xp; }
+EnemyType Enemy::Type() const { return enemyType; }
 
 void Enemy::attack(Entity& target) { target.takeDamage(Damage()); }
 
@@ -119,7 +176,7 @@ Enemy Enemy::Normal(int startX, int startY) {
     drops[0] = Utility::Wood().clone();
     drops[1] = Potion::SelfHealing().clone();
     drops[2] = nullptr;
-    return Enemy("Normal Enemy", startX, startY, 50, 5, std::move(drops), 10, 10);
+    return Enemy("Normal Enemy", startX, startY, 50, 5, std::move(drops), 10, 10, EnemyType::NORMAL);
 }
 
 Enemy Enemy::Tank(int startX, int startY) {
@@ -127,7 +184,7 @@ Enemy Enemy::Tank(int startX, int startY) {
     drops[0] = Utility::Stone().clone();
     drops[1] = Potion::Resistance().clone();
     drops[2] = nullptr;
-    return Enemy("Tank", startX, startY, 70, 15, std::move(drops), 15, 10);
+    return Enemy("Tank", startX, startY, 70, 15, std::move(drops), 15, 10, EnemyType::TANK);
 }
 
 Enemy Enemy::Poison(int startX, int startY) {
@@ -136,7 +193,7 @@ Enemy Enemy::Poison(int startX, int startY) {
     drops[1] = Utility::Wood().clone();
     drops[2] = nullptr;
     
-    Enemy poisonEnemy("Poison Enemy", startX, startY, 40, 5, std::move(drops), 12, 2);
+    Enemy poisonEnemy("Poison Enemy", startX, startY, 40, 5, std::move(drops), 12, 2, EnemyType::POISON);
     // Add permanent poison effect
     poisonEnemy.Effect_Add(Effect(EffectType::POISON, 999999, true));
     return poisonEnemy;
@@ -148,7 +205,7 @@ Enemy Enemy::Healer(int startX, int startY) {
     drops[1] = Potion::InflictHealing().clone();
     drops[2] = nullptr;
     
-    Enemy healerEnemy("Healer", startX, startY, 40, 5, std::move(drops), 8, 0);
+    Enemy healerEnemy("Healer", startX, startY, 40, 5, std::move(drops), 8, 0, EnemyType::HEALER);
     // Add permanent healing effect for others
     healerEnemy.Effect_Add(Effect(EffectType::HEAL, 999999, true));
     return healerEnemy;
@@ -160,7 +217,7 @@ Enemy Enemy::Wizard(int startX, int startY) {
     drops[1] = Potion::Vulnerability().clone();
     drops[2] = Utility::Iron().clone();
     
-    Enemy wizardEnemy("Wizard", startX, startY, 40, 5, std::move(drops), 15, 5);
+    Enemy wizardEnemy("Wizard", startX, startY, 40, 5, std::move(drops), 15, 5, EnemyType::WIZARD);
     // Add permanent weakness and vulnerability effects
     wizardEnemy.Effect_Add(Effect(EffectType::WEAKNESS, 999999, true));
     wizardEnemy.Effect_Add(Effect(EffectType::VULNERABLE, 999999, true));
