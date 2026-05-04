69d29e2170e69bcb5a67ad144c45bd74b3760eed
therealrahimkara2 <hss_kar_2733054@akesp.net>
2026-05-04 10:25:05 +0500

    Updated Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -5,25 +5,37 @@
 #include<array>
 #include<random>
 
+enum class EnemyType {
+    NORMAL,
+    TANK,
+    POISON,
+    HEALER,
+    WIZARD
+};
+
 class Enemy : public Entity {
     protected:
         std::array<std::unique_ptr<Item>, 3> drops;
         int xp;
         int damage;
         const int initialDamage;
+        EnemyType enemyType;
+        int tickCounter;
     public:
-        Enemy(std::string Name, int startX, int startY, int HP, int Defense, std::array<std::unique_ptr<Item>, 3> Drops, int XP, int Damage);
+        Enemy(std::string Name, int startX, int startY, int HP, int Defense, std::array<std::unique_ptr<Item>, 3> Drops, int XP, int Damage, EnemyType type);
         Enemy();
         Enemy(const Enemy& other);
-        Enemy(std::string Name, int startX, int startY, int HP, int Defense, int XP, int Damage, const std::array<std::unique_ptr<Item>, 3>& Drops);
+        Enemy(std::string Name, int startX, int startY, int HP, int Defense, int XP, int Damage, const std::array<std::unique_ptr<Item>, 3>& Drops, EnemyType type);
 
         void update(int x, int y) override;
+        void updateAI(const Entity& player);
         char getSymbol() const override;
 
         void attack(Entity& target);
         int Damage() const override;
         int Initial_Damage() const;
         size_t XP() const;
+        EnemyType Type() const;
 
         void Effect_Action(EffectType type) override;
 
