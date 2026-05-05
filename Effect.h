b92d89ada24a97d050290a9b94fabaaeccc201b3
BladedXeno <rahimkara147@gmail.com>
2026-05-05 15:36:52 +0500

    Updated Effect.h

--- Effect.h
+++ Effect.h
@@ -25,6 +25,7 @@ class Effect {
     public:
         Effect(EffectType type, double time, bool Can_Give_Others);
         Effect(const Effect& other);
+        Effect(); // Default constructor
         
         EffectType Effect_Give() const;
         double& Time();
