c3999eab586804ef9902f80b5898864db1b69cd9
BladedXeno <rahimkara147@gmail.com>
2026-05-05 15:36:28 +0500

    Updated Effect.cpp

--- Effect.cpp
+++ Effect.cpp
@@ -6,6 +6,9 @@ type(type), time(std::max(time, 0.0)), canGive(Can_Give_Others) {}
 Effect::Effect(const Effect& other) : 
 type(other.type), canGive(other.canGive), time(other.time) {}
 
+Effect::Effect() : 
+type(EffectType::HEAL), time(0.0), canGive(false) {}
+
 EffectType Effect::Effect_Give() const { return type; }
 
 double& Effect::Time() { return time; }
