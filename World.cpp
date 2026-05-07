5e94adf476a14bf49b0ed4c37a28e49053dd2edc
BladedXeno <rahimkara147@gmail.com>
2026-05-07 12:38:36 +0500

    Updated World.cpp

--- World.cpp
+++ World.cpp
@@ -1,48 +1,236 @@
 #include"World.h"
+#include"Enemy.h"
+#include"Boss.h"
 
 World::World() : 
-floors(7), totalFloors(7), activeFloor(nullptr) {}
+floors(5), totalFloors(5), activeFloor(nullptr) {}
 
 void World::SetActiveFloor(size_t FloorNumber) {
-    FloorNumber--;
-    FloorNumber = FloorNumber%7;
-    activeFloor = floors[FloorNumber].get();
+    if(FloorNumber == 0 || FloorNumber > totalFloors) {
+        activeFloor = nullptr;
+        return;
+    }
+    activeFloor = floors[FloorNumber - 1].get();
+}
+
+bool World::NextFloor() {
+    if(!activeFloor) {
+        return false;
+    }
+    int next = activeFloor->FloorNumber() + 1;
+    if(next > static_cast<int>(totalFloors)) {
+        return false;
+    }
+    SetActiveFloor(next);
+    return true;
+}
+
+int World::TotalFloors() const {
+    return static_cast<int>(totalFloors);
 }
 
 void World::InitializeFloor1() {
-    auto first = std::make_unique<Floor>(1, 2);
-    first->AddZone(36, 48);
-    first->AddZone(48, 48);
-    first->ZoneBoundary_andExits();
-    first->activeZone = first->zones[0].get();
-    for(int i = 0; i<first->Number_ofZones(); i++) {
+    auto floor = std::make_unique<Floor>(1, 2);
+    floor->AddZone(18, 18);
+    floor->AddZone(24, 24);
+    floor->ZoneBoundary_andExits();
+    floor->activeZone = floor->zones[0].get();
+    for(int i = 0; i<floor->Number_ofZones(); i++) {
         if(i == 0) {
-            first->zones[i]->ResourcePopulate(8, ResourceType::TREE);
-            first->zones[i]->ResourcePopulate(5, ResourceType::BARREL0);
+            floor->zones[i]->ResourcePopulate(8, ResourceType::TREE);
+            floor->zones[i]->ResourcePopulate(6, ResourceType::BARREL0);
         }
         else {
-            first->zones[i]->ResourcePopulate(15, ResourceType::TREE);
-            first->zones[i]->ResourcePopulate(4, ResourceType::STONE);
-            first->zones[i]->ResourcePopulate(5, ResourceType::BARREL1);
+            floor->zones[i]->ResourcePopulate(15, ResourceType::TREE);
+            floor->zones[i]->ResourcePopulate(4, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(5, ResourceType::BARREL1);
         }
     }
-    floors[0] = std::move(first);
+    floors[0] = std::move(floor);
 }
 
 void World::InitializeFloor2() {
-    // Implementation for Floor 2
+    auto floor = std::make_unique<Floor>(2, 2);
+    floor->AddZone(24, 24);
+    floor->AddZone(24, 24);
+    floor->ZoneBoundary_andExits();
+    floor->activeZone = floor->zones[0].get();
+    for(int i = 0; i<floor->Number_ofZones(); i++) {
+        if(i == 0) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(8, ResourceType::STONE);
+            floor->zones[i]->EntityPopulate(4, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Normal(Px, Py)); 
+            });
+        }
+        else {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(8, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(4, ResourceType::IRON);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Normal(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+        }
+    }
+    floors[1] = std::move(floor);
 }
 
 void World::InitializeFloor3() {
-    // Implementation for Floor 3
+    auto floor = std::make_unique<Floor>(3, 3);
+    floor->AddZone(24, 24);
+    floor->AddZone(24, 24);
+    floor->AddZone(30, 30);
+    floor->ZoneBoundary_andExits();
+    floor->activeZone = floor->zones[0].get();
+    for(int i = 0; i<floor->Number_ofZones(); i++) {
+        if(i == 0) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(8, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(3, ResourceType::IRON);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+        }
+        else if(i == 1) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(8, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(3, ResourceType::IRON);
+            floor->zones[i]->ResourcePopulate(5, ResourceType::BARREL2);
+            floor->zones[i]->EntityPopulate(3, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+        }
+        else {
+            floor->zones[i]->WallPopulate(6);
+            floor->zones[i]->EntityPopulate(3, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(1, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Boss>(Boss::PoisonousStoneGolem(Px, Py)); 
+            });
+        }
+    }
+    floors[2] = std::move(floor);
 }
 
 void World::InitializeFloor4() {
-    // Implementation for Floor 4
+    auto floor = std::make_unique<Floor>(4, 3);
+    floor->AddZone(24, 24);
+    floor->AddZone(24, 24);
+    floor->AddZone(24, 24);
+    floor->ZoneBoundary_andExits();
+    floor->activeZone = floor->zones[0].get();
+    for(int i = 0; i<floor->Number_ofZones(); i++) {
+        if(i == 0) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(10, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(4, ResourceType::IRON);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+        }
+        else if(i == 1) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(10, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(2, ResourceType::TITANIUM);
+            floor->zones[1]->ResourcePopulate(3, ResourceType::BARREL3);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+        }
+        else {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(10, ResourceType::STONE);
+            floor->zones[i]->ResourcePopulate(3, ResourceType::TITANIUM);
+            floor->zones[i]->ResourcePopulate(3, ResourceType::BARREL4);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+        }
+    }
+    floors[3] = std::move(floor);
 }
 
 void World::InitializeFloor5() {
-    // Implementation for Floor 5
+    auto floor = std::make_unique<Floor>(5, 4);
+    floor->AddZone(24, 24);
+    floor->AddZone(18, 18);
+    floor->AddZone(24, 24);
+    floor->AddZone(30, 30);
+    floor->ZoneBoundary_andExits();
+    floor->activeZone = floor->zones[0].get();
+    for(int i = 0; i<floor->Number_ofZones(); i++) {
+        if(i == 0) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Wizard(Px, Py)); 
+            });
+        }
+        else if(i == 1) {
+            floor->zones[i]->ResourcePopulate(8, ResourceType::TREE);
+            floor->zones[i]->EntityPopulate(1, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Poison(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Wizard(Px, Py)); 
+            });
+        }
+        else if(i == 2) {
+            floor->zones[i]->WallPopulate(5);
+            floor->zones[i]->ResourcePopulate(3, ResourceType::BARREL4);
+            floor->zones[i]->ResourcePopulate(2, ResourceType::BARREL5);
+            
+            floor->zones[i]->EntityPopulate(3, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Tank(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+        }
+        else {
+            floor->zones[i]->WallPopulate(6);
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Healer(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(2, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Enemy>(Enemy::Wizard(Px, Py)); 
+            });
+            floor->zones[i]->EntityPopulate(1, [](int Px, int Py) -> std::unique_ptr<Entity> { 
+                return std::make_unique<Boss>(Boss::EtheriteProtector(Px, Py)); 
+            });
+        }
+    }
+    floors[4] = std::move(floor);
 }
 
 void World::InitializeFloors() {
