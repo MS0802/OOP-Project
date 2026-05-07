020fb1bae16477a481ab28dd064ec5ba0a6d7973
BladedXeno <rahimkara147@gmail.com>
2026-05-07 12:26:48 +0500

    Updated Boss.h

--- Boss.h
+++ Boss.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"Enemy.h"
+#include<vector>
 
 class Boss : public Enemy {
     protected:
@@ -12,10 +13,14 @@ class Boss : public Enemy {
 
         void specialAttack(Entity& target);
 
+        std::pair<int,int> calculateAIMove(const Entity& player, const std::vector<std::unique_ptr<Entity>>& zoneEntities) const override;
+
         std::array<std::unique_ptr<Item>, 3> dropItem() override;
         
         // Clone method for creating copies
         std::unique_ptr<Entity> clone() const override;
+        
+        void updateAI(const Entity& player, const std::vector<std::unique_ptr<Entity>>& zoneEntities) override;
         
         // Static factory methods for boss types
         static Boss PoisonousStoneGolem(int startX, int startY);
