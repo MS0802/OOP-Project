3972fac4bc10a7cd20f791f08decc3502f5dd3f5
BladedXeno <rahimkara147@gmail.com>
2026-05-08 11:46:27 +0500

    Effect.cpp Completed

--- Effect.cpp
+++ Effect.cpp
@@ -14,3 +14,30 @@ EffectType Effect::Effect_Give() const { return type; }
 double& Effect::Time() { return time; }
 
 bool Effect::Can_Give() const { return canGive; }
+
+std::ostream& operator<<(std::ostream& os, const Effect& effect) {
+    os << "Effect Type: ";
+    switch(effect.type) {
+        case EffectType::HEAL:
+            os << "Heal";
+            break;
+        case EffectType::POISON:
+            os << "Poison";
+            break;
+        case EffectType::STRENGTH:
+            os << "Strength";
+            break;
+        case EffectType::WEAKNESS:
+            os << "Weakness";
+            break;
+        case EffectType::RESISTANCE:
+            os << "Resistance";
+            break;
+        case EffectType::VULNERABLE:
+            os << "Vulnerable";
+            break;
+    }
+    os << ", Time Remaining: " << effect.time << " seconds" 
+       << (effect.canGive ? ", Can Give to Others" : "");
+    return os;
+}
